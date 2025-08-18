462c3c3a4083e216f649d7d496845fce02593c2f
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-08-18 05:37:13 -0700

    refactor renderAutocompleteTaxon to handle missing fields

--- src/lib/inat_utils.ts
+++ src/lib/inat_utils.ts
@@ -1,15 +1,13 @@
 export type NormalizediNatTaxon = {
   name: string;
-  default_photo: string;
-  preferred_common_name: string;
+  default_photo?: string;
+  preferred_common_name?: string;
   matched_term: string;
   rank: string;
   id: number;
-  text: string;
-  value: number;
 };
 
-type iNatAutocompleteTaxa = {
+type iNatAutocompleteTaxaAPI = {
   total_results: number;
   page: number;
   per_page: number;
@@ -19,32 +17,95 @@ type iNatAutocompleteTaxa = {
 type iNatAutocompleteResult = {
   id: number;
   rank: string;
+  rank_level: number;
+  iconic_taxon_id: number;
+  ancestor_ids: number[];
+  is_active: boolean;
   name: string;
-  default_photo: {
-    square_url: string;
-  };
+  parent_id: number;
+  ancestry: string;
+  extinct: boolean;
+  default_photo: DefaultPhoto | null;
+  taxon_changes_count: number;
+  taxon_schemes_count: number;
   observations_count: number;
+  flag_counts: {
+    resolved: number;
+    unresolved: number;
+  };
+  current_synonymous_taxon_ids: number | null;
+  atlas_id: number | null;
+  complete_species_count: number | null;
+  wikipedia_url: string | null;
   matched_term: string;
-  preferred_common_name: string;
+  iconic_taxon_name: string;
+  preferred_common_name?: string;
+  conservation_status?: {
+    id: number;
+    place_id: number | null;
+    source_id: number | null;
+    user_id: number;
+    authority: string;
+    status: string;
+    status_name: string;
+    geoprivacy: string;
+    iucn: number;
+  };
 };
+
+type DefaultPhoto = {
+  id: number;
+  license_code: string | null;
+  attribution: string;
+  url: string;
+  original_dimensions: {
+    height: number;
+    width: number;
+  };
+  flags: string[];
+  attribution_name: string;
+  square_url: string;
+  medium_url: string;
+};
+
+const speciesRanks = ["species", "hybrid", "subspecies", "variety", "form"];
+
+export function formatTaxonName(
+  item: NormalizediNatTaxon,
+  inputValue: string,
+  includeMatchedTerm = true
+) {
+  let hasCommonName = true;
+  let title = item.preferred_common_name;
+  let titleAriaLabel = "taxon common name";
+  let subtitle;
+  let subtitleAriaLabel;
+
+  if (title) {
+    subtitle = item.name;
+    subtitleAriaLabel = "taxon scientific name";
+    if (
+      includeMatchedTerm &&
+      !title.toLowerCase().includes(inputValue.toLowerCase()) &&
+      !subtitle.toLowerCase().includes(inputValue.toLowerCase())
+    ) {
+      title += ` (${item.matched_term})`;
+    }
+  } else {
+    hasCommonName = false;
+    title = item.name;
+    titleAriaLabel = "taxon scientific name";
+  }
+
+  return { title, titleAriaLabel, subtitle, subtitleAriaLabel, hasCommonName };
+}
 
 export function renderAutocompleteTaxon(
   item: NormalizediNatTaxon,
   inputValue: string
 ): string {
-  let commonName = item.preferred_common_name;
-
-  if (!inputValue) {
-    return commonName;
-  }
-
-  if (
-    commonName &&
-    inputValue &&
-    !commonName.toLowerCase().includes(inputValue.toLowerCase())
-  ) {
-    commonName += ` (${item.matched_term})`;
-  }
+  let { title, titleAriaLabel, subtitle, subtitleAriaLabel, hasCommonName } =
+    formatTaxonName(item, inputValue);
 
   let html = `
   <div class="taxa-ac-option">
@@ -52,20 +113,23 @@ export function renderAutocompleteTaxon(
 
   if (item.default_photo) {
     html += `
-      <img class="thumbnail" src = "${item.default_photo}" >`;
+      <img class="thumbnail" src="${item.default_photo}" alt="">`;
   }
 
   html += `
     </div>
     <div class="taxon-name">
-      <span class="common-name" aria-label="taxon common name">${commonName}</span>
+      <span class="title" aria-label="${titleAriaLabel}">${title}</span>
       <span>`;
-  if (!["species", "variety", "form", "subspecies"].includes(item.rank)) {
+  if (!speciesRanks.includes(item.rank) || !hasCommonName) {
     html += `
         <span class="rank" aria-label="taxon rank">${item.rank}</span>`;
   }
+  if (hasCommonName) {
+    html += `
+        <span class="subtitle" aria-label="${subtitleAriaLabel}">${subtitle}</span>`;
+  }
   html += `
-        <span class="scientific-name" aria-label="taxon scientific name">${item.name}</span>
       </span>
     </div>
   </div>`;
@@ -74,7 +138,7 @@ export function renderAutocompleteTaxon(
 }
 
 export function processAutocompleteTaxa(
-  response: iNatAutocompleteTaxa
+  response: iNatAutocompleteTaxaAPI
 ): NormalizediNatTaxon[] {
   let taxa = response.results.map((result) => {
     return {
@@ -84,8 +148,6 @@ export function processAutocompleteTaxa(
       matched_term: result.matched_term,
       rank: result.rank,
       id: result.id,
-      text: result.matched_term,
-      value: result.id,
     };
   });
 
