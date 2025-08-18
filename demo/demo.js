0b261f02169ca984e35dd4002216e8bbecd4a9cd
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-08-17 21:08:54 -0700

    Revert "add web component demos to demo page"
    This reverts commit 24f2d251e6271834421f0f4fa3b40d3867f832a1.

--- demo/demo.js
+++
@@ -1,9 +0,0 @@
-import "./demo2.css";
-
-let app = document.querySelector("#app");
-if (app) {
-  app.innerHTML = `
-  <div class="class-one">Hello<span class="class-two">World</span> demo.js</div>
-  <p>Lorem ipsum</p>
-`;
-}
