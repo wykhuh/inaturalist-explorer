18339fe9c022e705b3184d382fe287344b43e4b6
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-08-18 18:05:52 -0700

    display and remove selected taxa

--- src/main.ts
+++ src/main.ts
@@ -20,15 +20,19 @@ import { displayJson } from "./lib/utils.ts";
 
 let api = "https://api.inaturalist.org/v1/taxa/autocomplete?q=";
 
-let selectedTaxa = [];
-let currentTaxon = {};
+let selectedTaxa: NormalizediNatTaxon[] = [];
 let inatTilesParams = {};
 
 let displayJsonEl = document.getElementById("display-json");
+let taxaListEl = document.getElementById("taxa-list-container");
+
+// =====================
+// taxa search
+// =====================
 
 const autoCompleteJS = new autoComplete({
   selector: "#inatTaxaAutoComplete",
-  placeHolder: "Search...",
+  placeHolder: "Enter species name",
   threshold: 3,
   searchEngine: (query: string, record: NormalizediNatTaxon) => {
     return renderAutocompleteTaxon(record, query);
@@ -53,7 +57,9 @@ const autoCompleteJS = new autoComplete({
   },
 });
 
-let { OpenStreetMap, OpenTopo } = getMapTiles();
+// =====================
+// taxa search
+// =====================
 
 let map = L.map("map", {
   center: [0, 0],
@@ -62,54 +68,100 @@ let map = L.map("map", {
 });
 var layerControl = L.control.layers().addTo(map);
 
+let { OpenStreetMap, OpenTopo } = getMapTiles();
 addLayerToMap(OpenStreetMap, map, layerControl, true);
 addLayerToMap(OpenTopo, map, layerControl);
 
 document
-  .querySelector("#inatTaxaAutoComplete")
+  .querySelector("#inatTaxaAutoComplete")!
   .addEventListener("selection", function (event) {
     let selection = event.detail.selection.value;
 
-    selectedTaxa.push(selection);
-    displayJson(selectedTaxa, displayJsonEl);
-    let bbValues = getBoundingBoxValues(map.getBounds());
-    inatTilesParams = {
-      ...inatTilesParams,
-      ...bbValues,
-      taxon_id: selection.id,
-    };
-    let paramsString = new URLSearchParams(inatTilesParams).toString();
-
-    let { iNatGrid, iNatHeatmap, iNatTaxonRange, iNatPoint } = getiNatMapTiles(
-      selection.id,
-      paramsString
-    );
-
-    let { title } = formatTaxonName(selection, event.detail.query, false);
-
-    let iNatGridLayer = addOverlayToLayerControl(
-      iNatGrid,
-      map,
-      layerControl,
-      title,
-      true
-    );
-    let iNatPointLayer = addOverlayToLayerControl(
-      iNatPoint,
-      map,
-      layerControl,
-      title
-    );
-    let iNatHeatmapLayer = addOverlayToLayerControl(
-      iNatHeatmap,
-      map,
-      layerControl,
-      title
-    );
-    let iNatTaxonRangeLayer = addOverlayToLayerControl(
-      iNatTaxonRange,
-      map,
-      layerControl,
-      title
-    );
+    taxonSelectedHandler(selection, event.detail.query);
   });
+
+// =====================
+// misc
+// =====================
+
+function removeTaxon(e) {
+  let taxonId = e.target.dataset.taxonId;
+  selectedTaxa = selectedTaxa.filter((taxon) => taxon.id !== Number(taxonId));
+  // document.querySelector(`.taxon-tag[data-taxon-id='${taxonId}']`)!.remove();
+  displayJson(selectedTaxa, displayJsonEl);
+  renderTaxaList(selectedTaxa, taxaListEl);
+}
+
+function renderTaxaList(
+  taxalist: NormalizediNatTaxon[],
+  containerEl: HTMLElement | null
+) {
+  if (!containerEl) return;
+  containerEl.innerHTML = "";
+
+  taxalist.forEach((taxon) => {
+    let spanEl = document.createElement("span");
+    spanEl.className = "taxon-tag";
+    spanEl.innerText = taxon.preferred_common_name || taxon.name;
+    spanEl.dataset.taxonId = taxon.id.toString();
+
+    let closeSpanEl = document.createElement("span");
+    closeSpanEl.innerText = "x";
+    closeSpanEl.className = "taxon-tag-close";
+    closeSpanEl.dataset.taxonId = taxon.id.toString();
+    closeSpanEl.addEventListener("click", removeTaxon);
+
+    spanEl.appendChild(closeSpanEl);
+    containerEl.appendChild(spanEl);
+  });
+}
+
+function taxonSelectedHandler(
+  taxonObj: NormalizediNatTaxon,
+  searchTerm: string
+) {
+  selectedTaxa.push(taxonObj);
+  displayJson(selectedTaxa, displayJsonEl);
+  renderTaxaList(selectedTaxa, taxaListEl);
+
+  let bbValues = getBoundingBoxValues(map.getBounds());
+  inatTilesParams = {
+    ...inatTilesParams,
+    ...bbValues,
+    taxon_id: taxonObj.id,
+  };
+  let paramsString = new URLSearchParams(inatTilesParams).toString();
+
+  let { iNatGrid, iNatHeatmap, iNatTaxonRange, iNatPoint } = getiNatMapTiles(
+    taxonObj.id,
+    paramsString
+  );
+
+  let { title } = formatTaxonName(taxonObj, searchTerm, false);
+
+  let iNatGridLayer = addOverlayToLayerControl(
+    iNatGrid,
+    map,
+    layerControl,
+    title,
+    true
+  );
+  let iNatPointLayer = addOverlayToLayerControl(
+    iNatPoint,
+    map,
+    layerControl,
+    title
+  );
+  let iNatHeatmapLayer = addOverlayToLayerControl(
+    iNatHeatmap,
+    map,
+    layerControl,
+    title
+  );
+  let iNatTaxonRangeLayer = addOverlayToLayerControl(
+    iNatTaxonRange,
+    map,
+    layerControl,
+    title
+  );
+}
