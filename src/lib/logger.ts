1ef5cab20df734e4265a099454cbf018ec6a5057
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-09-06 03:22:42 -0700

    add custom logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,20 @@
+export function logger(...args: any) {
+  const log = import.meta.env.VITE_LOGGER;
+  if (!log || log === "false") return;
+
+  console.log(...args);
+}
+
+export function loggerUrl(...args: any) {
+  const log = import.meta.env.VITE_LOGGER_URL;
+  if (!log || log === "false") return;
+
+  console.log(...args);
+}
+
+export function loggerFilters(...args: any) {
+  const log = import.meta.env.VITE_LOGGER_FILTERS;
+  if (!log || log === "false") return;
+
+  console.log(...args);
+}
