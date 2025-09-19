f79e4c40a81edee3304357be4f1fd1d91710ec22
wykhuh <wykhuh@users.noreply.github.com>
2025-09-19 07:52:58 +0000

    deploy: a41dc87f0118cdf415356441cbfc4d734a721b05

---
+++ assets/about-Cr83mIZF.js
@@ -0,0 +1,1 @@
+import"./component-BLWqEz9W.js";{let e=document.querySelector("head");if(e){let t=document.createElement("base");t.href="https://wykhuh.github.io/inaturalist-explorer/",e.appendChild(t)}}
--- assets/about-jvz1tCTe.js
+++
@@ -1,1 +0,0 @@
-import"./component-BtAyLo_o.js";{let e=document.querySelector("head");if(e){let t=document.createElement("base");t.href="https://wykhuh.github.io/inaturalist-explorer/",e.appendChild(t)}}
