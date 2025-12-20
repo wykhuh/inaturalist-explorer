954cfb5f182fe25249d6d146e9534a9164bf7f04
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-12-20 05:24:48 -0600

    update about page

--- src/components/Page404/template.ts
+++ src/components/Page404/template.ts
@@ -2,7 +2,7 @@ import { html } from "../../lib/component_utils";
 
 export const template = html`
   <site-header></site-header>
-  <main id="wrapper" class="flow">
+  <main id="wrapper" class="flow page-404">
     <h1>Page not found.</h1>
   </main>
 `;
--- src/components/PageAbout/template.ts
+++ src/components/PageAbout/template.ts
@@ -1,8 +1,15 @@
 import { html } from "../../lib/component_utils";
+import socal from "../../assets/images/search_socal_monarchs.png";
+import identifications from "../../assets/images/identifications.png";
+import grid from "../../assets/images/grid_subview.png";
+import table from "../../assets/images/table_subview.png";
+import media from "../../assets/images/media_subview.png";
+import annotations from "../../assets/images/annotations.png";
+import filters from "../../assets/images/filters.png";
 
 export const template = html`
   <site-header></site-header>
-  <main id="wrapper" class="flow">
+  <main id="wrapper" class="flow page-about">
     <h1>About</h1>
     <p>
       iNaturalist Explorer is a opensource website that lets people explore
@@ -13,52 +20,85 @@ export const template = html`
       page does not have.
     </p>
     <h2>Features</h2>
-    <ul>
-      <li>Search for multiple species, places, projects, and people</li>
+    <ol>
+      <li>
+        <a
+          href="/?taxon_id=48662,56851&place_id=962,829&colors=%234477aa,%2366ccee&verifiable=true&spam=false"
+          >Search for multiple</a
+        >
+        species, places, projects, and people
+      </li>
+      <li>
+        Show iNaturalist maps as grid, points, heatmap and taxon range
+        <img src="${socal}" />
+      </li>
+
+      <li>
+        View both observations and identifications
+        <img src="${identifications}" />
+      </li>
+      <li>
+        View observations by grid, table, or media
+        <img src="${grid}" />
+        <img src="${table}" />
+        <img src="${media}" />
+      </li>
       <li>Add more filters</li>
-      <li>Group the filters by categories</li>
+      <li>Group the filters by categories <img src="${filters}" /></li>
+      <li>Filter by annotations <img src="${annotations}" /></li>
       <li>Mobile friendly layout</li>
       <li>Pagination for observations, species, identifiers, and observers</li>
-      <li>Show iNaturalist maps as grid, points, heatmap and taxon range</li>
       <li>
-        This site gets data from the iNaturalist API. As a result, the urls for
-        the site are compatible with iNaturalist API. You can copy and paste the
-        query params (stuff after the ?) from the site, and use them for the
-        iNaturalist API. The only properties not compatible are colors, view,
-        and subview.
+        <p>
+          This site gets data from the iNaturalist API. As a result, the urls
+          for the site are compatible with iNaturalist API. You can copy and
+          paste the query params (stuff after the ?) from the site, and use them
+          for the iNaturalist API. The only properties not compatible are
+          colors, view, and subview.
+        </p>
 
         <p>
-          For instance, this is the URL query params for all monarchs in
-          California.
+          For instance, this is the URL query params for all monarchs and narrow
+          leaf milkweed in Los Angeles and San Diego.
         </p>
-        <div>
+        <p>
           <code
-            >taxon_id=48662&place_id=14&colors=%234477aa&verifiable=true&spam=false</code
+            >taxon_id=48662,56851&place_id=962,829&colors=%234477aa,%2366ccee&verifiable=true&spam=false</code
           >
-        </div>
+        </p>
         <p>
           You can copy the query params and use them for the
           <a
-            href="https://api.inaturalist.org/v1/observations?taxon_id=48662&place_id=14&colors=%234477aa&verifiable=true&spam=false"
+            href="https://api.inaturalist.org/v1/observations?taxon_id=48662,56851&place_id=962,829&colors=%234477aa,%2366ccee&verifiable=true&spam=false"
             >iNaturalist "observations" API</a
           >
         </p>
       </li>
-    </ul>
+    </ol>
+
     <h2>Techinical Details</h2>
     <p>
-      This static site is built using JavaScript/TypeScript, CSS, and HTML. I
-      wanted to use the built-in features of JavaScript, CSS, HTML, and keep
-      third party libraries to a minimum. This site uses TypeScript and three
-      libraries (<a href="https://leafletjs.com/">Leaflet</a>,
+      This static site is built using JavaScript/TypeScript, custom web
+      components, CSS, and HTML. I wanted to use the built-in features of
+      JavaScript, CSS, HTML, and keep third party libraries to a minimum. This
+      site uses Vite, TypeScript and three libraries (<a
+        href="https://leafletjs.com/"
+        >Leaflet</a
+      >,
       <a href="https://tarekraafat.github.io/autoComplete.js/"
         >Autocomplete.js</a
       >, and
       <a href="https://github.com/bramus/js-pagination-sequence"
         >Pagination Sequence</a
       >) to create site. This is the most complex project I've built without a
-      website or CSS framework. I also wanted to keep costs low as possible so
-      this is a static site that is hosted on Github.
+      website framework or CSS framework. I also wanted to keep costs low as
+      possible so this is a static site that is hosted for free on Cloudflare
+      Pages.
+    </p>
+    <p>
+      I did not use AI prompts or vibe coding to build this site. I wanted to
+      learn some new skills, such as custom web components, so I did some
+      research, learned a lot, and applied what I learned.
     </p>
     <ul>
       <li>
