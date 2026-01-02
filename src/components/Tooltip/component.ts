65f21ae950d3348709819bd7c6d3ef516b3be32d
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2026-01-02 11:49:39 -0600

    add tooltip component

---
+++ src/components/Tooltip/component.ts
@@ -0,0 +1,40 @@
+import { loggerRender } from "../../lib/logger";
+
+export class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  content = "";
+  tooltip = "";
+  id = "";
+
+  connectedCallback() {
+    loggerRender("++ Tooltip connectedCallback");
+
+    this.content =
+      this.getAttribute("data-content") || "Tooltip content not defined";
+    this.tooltip = this.getAttribute("data-tooltip") || "Tooltip not defined";
+    this.id = this.getAttribute("data-id") || "Tooltip id not defined";
+
+    this.render();
+  }
+
+  render() {
+    loggerRender("++ Tooltip render");
+
+    let content = formatTooltip(this.id, this.content, this.tooltip);
+
+    this.innerHTML = content;
+  }
+}
+
+customElements.define("app-tooltip", Tooltip);
+
+export function formatTooltip(id: string, content: string, tooltip: string) {
+  let html = '<div class="tp-wrapper">';
+  html += `<span class="btn-borderless tp-trigger" aria-describedby="${id}">${content}</span>`;
+  html += `<p id='${id}' role='tooltip'>${tooltip}</p>`;
+  html += "</div>";
+  return html;
+}
--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ import "./components/AppstoreViewer/component.ts";
 import "./components/Header/component.ts";
 import "./components/Page404/component.ts";
 import "./components/Pagination/component.ts";
+import "./components/Tooltip/component.ts";
 
 import mapStore from "./lib/store.ts";
 import Router from "./lib/router.ts";
