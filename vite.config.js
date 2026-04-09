d74902f2f7885c7ededee20c9581517e4e7eda3c
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2026-04-09 12:53:12 -0700

    remove console.log

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,6 @@ export default defineConfig({
       },
       output: {
         entryFileNames: (assetInfo) => {
-          console.log(assetInfo.name);
-          // return "--[name]";
           return assetInfo.name === "service_worker"
             ? "[name].js" // put service worker in root
             : "assets/[name]-[hash].js"; // others in `assets/`
